66ce1bf1f67df95a195fd448ef342653ae271d78
daviddiz <daviddiz@paradigmadigital.com>
2021-02-07 15:47:57 +0100

    init commit

---
+++ src/main/java/com/example/documenttracking/DocumentTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.documenttracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/documenttracking/DocumentTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documenttracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
