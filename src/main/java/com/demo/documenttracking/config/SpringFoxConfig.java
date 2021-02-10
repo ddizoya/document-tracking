29b3c747dee55e9f08ee7180ae978fc09d92a0ad
daviddiz <daviddiz@paradigmadigital.com>
2021-02-10 15:03:19 +0100

    Swagger now only cares about RestControllers

--- src/main/java/com/demo/documenttracking/config/SpringFoxConfig.java
+++ src/main/java/com/demo/documenttracking/config/SpringFoxConfig.java
@@ -3,6 +3,7 @@ package com.demo.documenttracking.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.paths.RelativePathProvider;
@@ -20,7 +21,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build()
                 .pathProvider(new RelativePathProvider(servletContext) {
 
